d3f854c187a2c8a1e5f672a53739ca780e71e149
Maria Rosario <mariarosariojohansson@gmail.com>
2022-03-22 11:57:37 +0100

    just for funzies

--- itsgettingdicey.js
+++ itsgettingdicey.js
@@ -2,7 +2,6 @@ document.getElementById('rollTheDiceBtn').addEventListener('click', startGame);
 
 function startGame() {
     const throwDice = getRandomNum();
-    console.log(throwDice);
     let imgTag = document.getElementById('diceImg');
     imgTag.src = `src/img/dice-${throwDice}.png`
 }
