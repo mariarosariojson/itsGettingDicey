064e1444697a9a17e1390fd87dd19b990962759b
Maria Rosario <mariarosariojohansson@gmail.com>
2022-03-30 11:46:50 +0200

    work in progress ...

--- itsgettingdicey.js
+++ itsgettingdicey.js
@@ -1,19 +1,23 @@
-document.getElementById("rollTheDiceBtn").addEventListener("click", startGame);
+// document.getElementById("rollTheDiceBtn").addEventListener("click", startGame);
 
 function startGame() {
   const throwDiceOne = getRandomNumOne();
   const throwDiceTwo = getRandomNumTwo();
   const totalScore = document.getElementById("totalScore");
   let diceTotal = throwDiceOne + throwDiceTwo;
-  console.log(diceTotal);
+  console.log(`total score: ${diceTotal}`);
   totalScore.innerHTML = `<span style='font-size: 40px; color: grey;'>total score is: ${diceTotal}</span>`;
 
   let imgTagOne = document.getElementById("diceImgOne");
   imgTagOne.src = `src/img/dice-${throwDiceOne}.png`;
   let imgTagTwo = document.getElementById("diceImgTwo");
   imgTagTwo.src = `src/img/dice-${throwDiceTwo}.png`;
-  if (throwDiceOne == throwDiceTwo) {
-    totalScore.innerHTML = `<span style='font-size: 40px; color: green;'>It's a pair!!</span>`;
+
+  if (inputNumber.value == diceTotal) {
+    console.log(`It's a MATCH`);
+    inputNumber.value.innerHTML = `<span style='font-size: 40px; color: green;'>It's a pair!!</span>`;
+  } else {
+    console.log(`It's NOT a match`);
   }
 }
 
